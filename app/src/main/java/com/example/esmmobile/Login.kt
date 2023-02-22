2c36dfa5cfdba24712b5076fc404543792ad1bf2
leo <108675763+Etilaleo@users.noreply.github.com>
2023-02-22 01:53:59 +0100

    fresh commits

---
+++ app/src/main/java/com/example/esmmobile/Login.kt
@@ -0,0 +1,13 @@
+package com.example.esmmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
+    }
+}
