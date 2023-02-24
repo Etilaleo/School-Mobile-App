4ba860498c7b439498b9a56716f5a9d364a1a435
leo <108675763+Etilaleo@users.noreply.github.com>
2023-02-24 21:18:21 +0100

    new commit

---
+++ app/src/main/java/com/example/esmmobile/screens/EventsFragment.kt
@@ -0,0 +1,25 @@
+package com.example.esmmobile.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.esmmobile.databinding.FragmentEventsBinding
+
+class EventsFragment : Fragment() {
+
+    private lateinit var binding: FragmentEventsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentEventsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/esmmobile/screens/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.esmmobile.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.denzcoskun.imageslider.models.SlideModel
+import com.example.esmmobile.R
+import com.example.esmmobile.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imageSliderList = ArrayList<SlideModel>()
+        imageSliderList.add(SlideModel(R.drawable.esm_logo))
+
+        binding.imageSlider.setImageList(imageSliderList)
+    }
+}
