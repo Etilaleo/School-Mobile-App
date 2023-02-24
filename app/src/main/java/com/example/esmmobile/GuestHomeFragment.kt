59d220c1ab86ce826bb58239c0369bdcfc0d76e8
leo <108675763+Etilaleo@users.noreply.github.com>
2023-02-24 21:16:44 +0100

    new commit

--- app/src/main/java/com/example/esmmobile/GuestHomeFragment.kt
+++ app/src/main/java/com/example/esmmobile/GuestHomeFragment.kt
@@ -5,17 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.esmmobile.databinding.FragmentEventsBinding
+import com.example.esmmobile.databinding.FragmentGuestHomeBinding
 
-class EventsFragment : Fragment() {
+class GuestHomeFragment : Fragment() {
 
-    private lateinit var binding: FragmentEventsBinding
+    private lateinit var binding : FragmentGuestHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        binding = FragmentEventsBinding.inflate(layoutInflater)
+        binding = FragmentGuestHomeBinding.inflate(layoutInflater)
         return binding.root
     }
 
---
+++ app/src/main/java/com/example/esmmobile/GuestPages.kt
@@ -0,0 +1,38 @@
+package com.example.esmmobile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.esmmobile.databinding.ActivityGuestPagesBinding
+
+class GuestPages : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGuestPagesBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGuestPagesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        setSupportActionBar(binding.toolbar)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+        setupActionBarWithNavController(navController, binding.drawerLayout)
+        binding.navView.setupWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        return navController.navigateUp(appBarConfiguration) ||  super.onSupportNavigateUp()
+    }
+}
